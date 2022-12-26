bc2c97e4d8983123542c7e6a9782d359fadecf18
unknown <kusalkalingainfo@gmail.com>
2022-12-26 20:05:00 +0530

    First Commit-data showing from Json file

---
+++ components/updateUserForm.js
@@ -0,0 +1,110 @@
+import { useReducer } from "react";
+import { BiBrush } from "react-icons/bi";
+import Success from "./success";
+import Error from "./error";
+
+const formReducer = (state, event) => {
+  return {
+    ...state,
+    [event.target.name]: event.target.value,
+  };
+};
+
+export default function UpdateUserForm() {
+  const [formData, setFormData] = useReducer(formReducer, {});
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (Object.keys(formData).length == 0)
+      return console.log("Dont have form Data");
+    console.log(formData);
+  };
+
+  if (Object.keys(formData).length > 0)
+    return <Success message={"Data Added"}></Success>;
+
+  return (
+    <form className="grid lg:grid-cols-2 w-4/6 gap-4" onSubmit={handleSubmit}>
+      <div className="input-type">
+        <input
+          type="text"
+          name="firstname"
+          className="border w-full px-5 py-3 focus:outline-none rounded-md"
+          placeholder="First Name"
+          onChange={setFormData}
+        />
+      </div>
+      <div className="input-type">
+        <input
+          type="text"
+          name="lastname"
+          className="border w-full px-5 py-3 focus:outline-none rounded-md"
+          placeholder="Last Name"
+          onChange={setFormData}
+        />
+      </div>
+      <div className="input-type">
+        <input
+          type="text"
+          name="email"
+          className="border w-full px-5 py-3 focus:outline-none rounded-md"
+          placeholder="Email"
+          onChange={setFormData}
+        />
+      </div>
+      <div className="input-type">
+        <input
+          type="text"
+          name="salary"
+          className="border w-full px-5 py-3 focus:outline-none rounded-md"
+          placeholder="Salary"
+          onChange={setFormData}
+        />
+      </div>
+      <div className="input-type">
+        <input
+          type="date"
+          name="date"
+          className="border px-5 py-3 focus:outline-none rounded-md"
+          onChange={setFormData}
+        />
+      </div>
+
+      <div className="flex gap-10 items-center">
+        <div className="form-check">
+          <input
+            type="radio"
+            name="status"
+            className="form-check-input appearance-none  rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-green-500 checked:border-green-500 focus:outline-none transition duration-200 mt-1 top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+            value="Active"
+            id="radioDefault1"
+            onChange={setFormData}
+          />
+          <label htmlFor="radioDefault1" className="inline-block text-gray-800">
+            Active
+          </label>
+        </div>
+
+        <div className="form-check">
+          <input
+            type="radio"
+            name="status"
+            className="form-check-input appearance-none  rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-green-500 checked:border-green-500 focus:outline-none transition duration-200 mt-1 top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+            value="Inactive"
+            id="radioDefault2"
+            onChange={setFormData}
+          />
+          <label htmlFor="radioDefault2" className="inline-block text-gray-800">
+            Inactive
+          </label>
+        </div>
+      </div>
+      <button className="flex justify-center text-md w-2/6 bg-yellow-400 text-white px-4 py-2 border rounded-md hover:bg-gray-50 hover:border-yellow-500 hover:text-yellow-500">
+        Update{" "}
+        <span className="px-1">
+          <BiBrush size={24} />
+        </span>
+      </button>
+    </form>
+  );
+}
